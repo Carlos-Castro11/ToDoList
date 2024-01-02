e4b67468c214eca4df7b2ba7ed83b5821383fc57
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-01-02 16:40:18 -0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,86 @@
+﻿Console.WriteLine("Welcome to the ===CALCULATOR===");
+
+Console.WriteLine("Enter the first number");
+string string1 = Console.ReadLine();
+//var numberTyped1 = int.Parse(string1);
+int numberTyped1 = 0;
+if (int.TryParse(string1, out numberTyped1))
+{
+    Console.WriteLine("Valid number");
+}
+else
+{
+    Console.WriteLine("Invalid value!");
+    return;
+}
+
+Console.WriteLine("Enter the second number");
+string string2 = Console.ReadLine();
+//var numberTyped2 = int.Parse(string2);
+int numberTyped2 = 0;
+if (int.TryParse(string2, out numberTyped2))
+{
+    Console.WriteLine("Valid value");
+}
+else
+{
+    Console.WriteLine("Invalid value");
+    return;
+}
+
+Console.WriteLine("Select what operation do you want: ");
+Console.WriteLine("[S] Sum");
+Console.WriteLine("[T] Subtraction");
+Console.WriteLine("[M] Multiplication");
+Console.WriteLine("[D] Division");
+var chosenOperation = Console.ReadLine().ToLower();
+
+
+switch(chosenOperation)
+{
+    case "s":
+        var resSum = Sum(numberTyped1, numberTyped2);
+        Console.WriteLine($"O resultado da soma entre {numberTyped1} e {numberTyped2} é {resSum}");
+        break;
+
+    case "t":
+        var resSubtraction = Subtraction(numberTyped1, numberTyped2);
+        Console.WriteLine($"O resultado da subtração entre {numberTyped1} e {numberTyped2} é {resSubtraction}");
+        break;
+
+    case "m":
+        var resMultiplication = Multiplication(numberTyped1, numberTyped2);
+        Console.WriteLine($"O resultado da multiplicação entre {numberTyped1} e {numberTyped2} é {resMultiplication}");
+        break;
+
+    case "d":
+        var resDivision = Division(numberTyped1, numberTyped2);
+        Console.WriteLine($"O resultado da divisão entre {numberTyped1} e {numberTyped2} é {resDivision}");
+        break;
+
+    default:
+        Console.WriteLine("Invalid option!");
+        break;
+}
+
+Console.ReadKey();
+
+int Sum(int number1, int number2)
+{
+    return number1 + number2;
+}
+
+int Subtraction(int number1, int number2)
+{
+    return number1 - number2;
+}
+
+int Multiplication(int number1, int number2)
+{
+    return number1 * number2;
+}
+
+int Division(int number1, int number2)
+{
+    return (number1 / number2);
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Threading.Tasks;
+
+string userChoice;
+bool isValid = true;
+List<string> tasks = new List<string>();
+
+do
+{
+    Console.WriteLine("Hello!");
+    Console.WriteLine("\n");
+    Console.WriteLine("[S] See all toDos");
+    Console.WriteLine("[A] Add a toDo");
+    Console.WriteLine("[R] Remove a toDo");
+    Console.WriteLine("[E] Exit");
+    Console.WriteLine("\n");
+    Console.WriteLine("Choose an option: ");
+    userChoice = Console.ReadLine().ToLower();
+    switch (userChoice)
+    {
+        case "s":
+            Console.WriteLine("\n");
+            isValid = true;
+            showtTasks(tasks);
+            break;
+        case "a":
+            Console.WriteLine("\n");
+            isValid = true;
+            addTask();
+            break;
+
+        case "r":
+            Console.WriteLine("\n");
+            isValid = true;
+            removeTask(tasks);
+            break;
+
+        case "e":
+            Console.WriteLine("\n");
+            isValid = true;
+            return;
+
+        default:
+            Console.WriteLine("Invalid answer");
+            isValid = false;
+            break;
+    }
+} while (!isValid);
+
+Console.ReadKey();
+
+void addTask()
+{
+    Console.WriteLine("set a Task name: ");
+    string task = Console.ReadLine();
+    tasks.Add(task);
+    isValid = false;
+    Console.WriteLine("\n");
+
+}
+
+void showtTasks(List<string> tasks)
+{
+    Console.WriteLine("================== Showing all toDo List ======================");
+    tasks.ForEach(task =>
+    {
+        task.ToUpper();
+        var index = tasks.IndexOf(task);
+        Console.WriteLine($"{index + 1} - {task}");
+    });
+    Console.WriteLine("\n");
+    isValid = false;
+}
+
+void removeTask(List<string> tasks)
+{
+    Console.WriteLine("Type the id for remove the task: ");
+    var idTaskOnStringFormat = Console.ReadLine();
+    var idTaskOnIntFormat = int.Parse(idTaskOnStringFormat);
+    tasks.ForEach(task =>
+    {
+        var index = tasks.IndexOf(task);
+        Console.WriteLine($"{task} e index dessa task é {index}");
+    });
+    isValid = false;
+}
